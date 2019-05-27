15fa2d19396f91e04dbd0e1d0b9af4a7345901f1
Yaroslav Lichadeyev <yaroslav.lichadeyev@id-east.ru>
2019-05-27 16:10:41 +0300

    Задание первого урока

--- app/src/main/java/com/example/rumpilstilstkin/lesson1/moxy/Presenter.java
+++ app/src/main/java/com/example/rumpilstilstkin/lesson1/moxy/Presenter.java
@@ -5,7 +5,6 @@ import android.util.Log;
 
 import com.arellomobile.mvp.InjectViewState;
 import com.arellomobile.mvp.MvpPresenter;
-import com.example.rumpilstilstkin.lesson1.R;
 
 @InjectViewState
 public class Presenter extends MvpPresenter<MoxyExampleView> {
@@ -30,24 +29,29 @@ public class Presenter extends MvpPresenter<MoxyExampleView> {
         return currentValue + 1;
     }
 
-    public void buttonClick(final int btnIndex) {
-        int newModelValue;
-        switch (btnIndex) {
-            case R.id.btnCounter1:
-                newModelValue = calcNewModelValue(0);
-                mModel.setElementValueAtIndex(0, newModelValue);
-                getViewState().setButtonText(1, newModelValue);
-                break;
-            case R.id.btnCounter2:
-                newModelValue = calcNewModelValue(1);
-                mModel.setElementValueAtIndex(1, newModelValue);
-                getViewState().setButtonText(2, newModelValue);
-                break;
-            case R.id.btnCounter3:
-                newModelValue = calcNewModelValue(2);
-                mModel.setElementValueAtIndex(2, newModelValue);
-                getViewState().setButtonText(3, newModelValue);
-                break;
-        }
+//    public void buttonClick(final int btnIndex) {
+//        int newModelValue;
+//        switch (btnIndex) {
+//            case R.id.btnCounter1:
+//                newModelValue = calcNewModelValue(0);
+//                mModel.setElementValueAtIndex(0, newModelValue);
+//                getViewState().setButtonText(1, newModelValue);
+//                break;
+//            case R.id.btnCounter2:
+//                newModelValue = calcNewModelValue(1);
+//                mModel.setElementValueAtIndex(1, newModelValue);
+//                getViewState().setButtonText(2, newModelValue);
+//                break;
+//            case R.id.btnCounter3:
+//                newModelValue = calcNewModelValue(2);
+//                mModel.setElementValueAtIndex(2, newModelValue);
+//                getViewState().setButtonText(3, newModelValue);
+//                break;
+//        }
+//    }
+    public int buttonClick(int modelElementIndex){
+        int newModelValue = calcNewModelValue(modelElementIndex);
+        mModel.setElementValueAtIndex(modelElementIndex, newModelValue);
+        return newModelValue;
     }
 }
